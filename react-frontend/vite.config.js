5cbc08f34bf2b03164b590468b3bae856497cd8d
AbdulRahim012 <ar818.abdulrahim@gmail.com>
2025-12-25 23:11:41 +0530

    Fixed Vercel

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
